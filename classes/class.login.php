c80567b824f22e0288b0a11d6d850f903df27aff
Timothy Dunn <tdunn@td14.com>
2010-08-05 04:12:39 -0500

    folders for project

---
+++ classes/class.login.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Timothy Dunn
+ * @copyright 2010
+ */
+
+ session_start();
+ 
+include_once("class.database.php");
+ 
+ class login{
+	private $db;
+    
+    public function __construct($tmp_info_db, $server_tmp = "localhost"){
+        if(is_array($tmp_info_db)){
+            $this->db = new database($tmp_info_db, $server_tmp);            
+        } else {
+            return false;
+        }
+    }
+    
+ }
+
+?>
